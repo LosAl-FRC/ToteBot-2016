75a0dd32da92227bc71173f96b788112662e796b
Carter Gale <buildfresh@gmail.com>
2016-10-13 12:08:10 -0700

    Initial

---
+++ src/main/java/org/ucfirst/frc/team6220/Robot.java
@@ -0,0 +1,33 @@
+package org.ucfirst.frc.team6220;
+
+import edu.wpi.first.wpilibj.*;
+
+/**
+ * Created by CGale9024 on 10/12/2016.
+ */
+public class Robot extends IterativeRobot {
+
+    //Defines the variables as members of our Robot class
+    RobotDrive drive; //pins 0,1,2,3
+    Joystick joystick;
+    Servo steering; //channel 4
+    int autoLoopCounter;
+
+    //Initializes the variables in the robotInit method, this method is called when the robot is initializing
+    public void robotInit() {
+        this.drive = new RobotDrive(0,1,2,3);
+        this.joystick = new Joystick(1);
+        this.steering = new Servo(4);
+    }
+
+    public void operatorControl() {
+        //int reverse = 1;
+        while(isOperatorControl() && isEnabled()) {
+            //http://team358.org/files/programming/ControlSystem2015-2019/images/XBoxControlMapping.jpg
+            steering.setAngle((joystick.getRawAxis(0) + 1) / 2);
+            drive.drive(,0);
+            Timer.delay(0.005);
+        }
+    }
+
+}
