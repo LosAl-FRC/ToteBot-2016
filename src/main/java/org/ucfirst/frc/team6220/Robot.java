0206744b84b083b4965f9bb52782e70f93c71c11
Ktar5 <buildfresh@gmail.com>
2016-10-13 21:29:25 -0700

    Proper header and formatting

--- src/main/java/org/ucfirst/frc/team6220/Robot.java
+++ src/main/java/org/ucfirst/frc/team6220/Robot.java
@@ -3,7 +3,10 @@ package org.ucfirst.frc.team6220;
 import edu.wpi.first.wpilibj.*;
 
 /**
- * Created by CGale9024 on 10/12/2016.
+ * @author Ktar5 (Carter Gale)
+ * @author RolyPoly (Will Dang)
+ *
+ *         Creation date: 10/13/16
  */
 public class Robot extends IterativeRobot {
 
@@ -15,14 +18,14 @@ public class Robot extends IterativeRobot {
 
     //Initializes the variables in the robotInit method, this method is called when the robot is initializing
     public void robotInit() {
-        this.drive = new RobotDrive(0,1,2,3);
+        this.drive = new RobotDrive(0, 1, 2, 3);
         this.joystick = new Joystick(1);
         this.steering = new Servo(4);
     }
 
     public void operatorControl() {
         //int reverse = 1;
-        while(isOperatorControl() && isEnabled()) {
+        while (isOperatorControl() && isEnabled()) {
             //http://team358.org/files/programming/ControlSystem2015-2019/images/XBoxControlMapping.jpg
             steering.setAngle((joystick.getRawAxis(0) + 1) / 2);
             double movement = joystick.getRawAxis(3) - joystick.getRawAxis(2);
@@ -32,3 +35,4 @@ public class Robot extends IterativeRobot {
     }
 
 }
+
