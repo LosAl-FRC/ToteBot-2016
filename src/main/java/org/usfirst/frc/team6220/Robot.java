6764a1bb1b4b29bdd5c9cff9de4695319f9fb04b
Ktar5 <buildfresh@gmail.com>
2016-10-26 22:21:50 -0700

    De-cancered the formatting and comments

--- src/main/java/org/usfirst/frc/team6220/Robot.java
+++ src/main/java/org/usfirst/frc/team6220/Robot.java
@@ -11,12 +11,11 @@ import edu.wpi.first.wpilibj.*;
  */
 public class Robot extends SampleRobot {
 
-    //Defines the variables as members of our Robot class
-    RobotDrive drive; //pins 0,1,2,3
-    Joystick joystick;
-    Servo steering; //channel 4
+    private RobotDrive drive; //pins 0,1,2,3
+    private Joystick joystick; //port bottom right on Carter's laptop
+    private Servo steering; //pin 4
 
-    //Initializes the variables in the robotInit method, this method is called when the robot is initializing.
+    //Called when the robot is initialized
     public void robotInit() {
         System.out.println("Robot has started");
         this.drive = new RobotDrive(0, 1, 2, 3);
@@ -24,22 +23,24 @@ public class Robot extends SampleRobot {
         this.steering = new Servo(4);
     }
 
+    //http://team358.org/files/programming/ControlSystem2015-2019/images/XBoxControlMapping.jpg
     public void operatorControl() {
-        //int reverse = 1;
         int i = 0;
         while (isOperatorControl() && isEnabled()) {
-            //http://team358.org/files/programming/ControlSystem2015-2019/images/XBoxControlMapping.jpg
             double angle = (joystick.getRawAxis(0) + 1) / 2;
             if(angle > .48 && angle < .51){
                 angle = .5;
             }
-            angle *= 180;
-            //Fun things v
+            angle *= 15;
             //angle = Math.pow(angle, joystick.getRawAxis(3) - joystick.getRawAxis(2));
             steering.setAngle(angle);
             double movement = joystick.getRawAxis(3) - joystick.getRawAxis(2);
             drive.drive(movement, 0);
-            Timer.delay(0.005);
+            /*
+            drive.setInvertedMotor(RobotDrive.MotorType.kFrontLeft, true);
+            drive.setInvertedMotor(RobotDrive.MotorType.kFrontRight, true);
+            */
+            Timer.delay(0.01);
             if(i % 50 == 0){
                 i = 0;
                 System.out.println("Movement: " + movement + "\nAngle: " + angle + "\n");
@@ -47,6 +48,7 @@ public class Robot extends SampleRobot {
             i++;
         }
     }
+
 
 }
 
